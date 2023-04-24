9b55d62e5d1647e724e63296e8daacafb2c29fc5
superdev0826 <superdev0826@gmail.com>
2023-04-24 08:00:46 -0400

    main

---
+++ vue/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+ <div class="title">
+    <h2>Vue2 on Total.js Platform</h2>
+
+  <v-carousel>
+    <v-carousel-item v-for="(src, i) in images" v-bind:src="src" :key="i"></v-carousel-item>
+  </v-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    images: [
+      'https://vuetifyjs.com/static/doc-images/carousel/squirrel.jpg',
+      'https://vuetifyjs.com/static/doc-images/carousel/sky.jpg',
+      'https://vuetifyjs.com/static/doc-images/carousel/bird.jpg',
+      'https://vuetifyjs.com/static/doc-images/carousel/planet.jpg'
+    ]
+  })
+}
+</script>
